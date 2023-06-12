e5b65bcc32be1e41d4374b29e4c5a39fd28a4ad9
Luís Mendes <jluis.lam1@gmail.com>
2023-06-12 23:04:30 +0100

    Fragment2

--- app/src/main/java/pt/ipt/dama/dama_threefragmentscamera/fragments/Fragment2.kt
+++ app/src/main/java/pt/ipt/dama/dama_threefragmentscamera/fragments/Fragment2.kt
@@ -1,5 +1,7 @@
 package pt.ipt.dama.dama_threefragmentscamera.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
@@ -11,19 +13,22 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import pt.ipt.dama.dama_threefragmentscamera.R
 import java.io.FileDescriptor
 import java.io.IOException
 
+@Suppress("DEPRECATION")
 class Fragment2 : Fragment() {
     private lateinit var frame: ImageView
     private var imageUri: Uri? = null
     private val RESULT_LOAD_IMAGE = 123
     private val IMAGE_CAPTURE_CODE = 654
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,18 +41,35 @@ class Fragment2 : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.fragment2Button)
         // Use the button reference to set listeners, change properties, etc.
-        frame = view.findViewById<ImageView>(R.id.fragment2imageView)
+        frame = view.findViewById(R.id.fragment2frame)
 
-        // we are going to do something useful
-        frame?.setOnLongClickListener {
+        // check if you can use the camera and write the image to storage
+        val context = requireContext() // had to be modified to work properly
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED ||
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            // we have a problem. One, or both, permission were not given
+            val permission = arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            ActivityCompat.requestPermissions(requireActivity(), permission, 112)
+        }
+
+        frame.setOnLongClickListener {
             // the user press for long time the imageView
             openCamera()
             true
         }
 
-        frame?.setOnClickListener {
+        frame.setOnClickListener {
             // the user only touch the imageView
             openGallery()
         }
@@ -67,41 +89,49 @@ class Fragment2 : Fragment() {
      */
     private fun openCamera() {
         val values = ContentValues()
-        values.put(MediaStore.Images.Media.TITLE, "new picture")
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
         values.put(MediaStore.Images.Media.DESCRIPTION, "Image from the camera")
-        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        imageUri = requireActivity().contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values
+        )
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
         startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
     }
 
+
     // *********************************************
     // auxiliary functions
     // *********************************************
+
 
     /**
      * Reads a bitmap file
      */
     private fun uriToBitmap(selectedFileUri: Uri): Bitmap? {
-        try {
-            val parcelFileDescriptor =
-                contentResolver.openFileDescriptor(selectedFileUri, "r")
+        return try {
+            val parcelFileDescriptor = // Fixes contentResolver
+                requireActivity().contentResolver.openFileDescriptor(selectedFileUri, "r")
             val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
             val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
             parcelFileDescriptor.close()
-            return image
+            image
         } catch (e: IOException) {
             e.printStackTrace()
-            return null
+            null
         }
     }
 
+
+
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == IMAGE_CAPTURE_CODE && resultCode == Activity.RESULT_OK) {
             val bitmap = uriToBitmap(imageUri!!)
-            frame?.setImageBitmap(bitmap)
+            frame.setImageBitmap(bitmap)
         }
         if (requestCode == RESULT_LOAD_IMAGE &&
             resultCode == Activity.RESULT_OK &&
@@ -109,7 +139,7 @@ class Fragment2 : Fragment() {
         ) {
             imageUri = data.data
             val bitmap = uriToBitmap(imageUri!!)
-            frame?.setImageBitmap(bitmap)
+            frame.setImageBitmap(bitmap)
         }
 
     }
